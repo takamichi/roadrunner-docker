9ddf40ce7f1c1d85ab91fb1d31c7f2d1ea91003f
Takamichi Urata <taka@seraphimis.net>
2019-05-23 07:48:04 +0900

    clean global

--- backend/worker.php
+++ backend/worker.php
@@ -3,15 +3,21 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-$relay = new Spiral\Goridge\StreamRelay(STDIN, STDOUT);
-$psr7 = new Spiral\RoadRunner\PSR7Client(new Spiral\RoadRunner\Worker($relay));
+(function () {
+    $psr7 = new Spiral\RoadRunner\PSR7Client(
+        new Spiral\RoadRunner\Worker(new Spiral\Goridge\StreamRelay(STDIN, STDOUT))
+    );
 
-while ($request = $psr7->acceptRequest()) {
-    try {
-        $response = new \Zend\Diactoros\Response();
-        $response->getBody()->write('Hello world from RoadRunner!');
-        $psr7->respond($response);
-    } catch (\Throwable $e) {
-        $psr7->getWorker()->error((string)$e);
+    while ($request = $psr7->acceptRequest()) {
+        $ulid = Ulid\Ulid::generate();
+        $request = $request->withAddedHeader('R2-Request-Id', (string)$ulid);
+
+        try {
+            $response = (new Zend\Diactoros\Response())->withAddedHeader('R2-Request-Id', (string)$ulid);
+            $response->getBody()->write('Hello world from RoadRunner! ' . (string)$ulid);
+            $psr7->respond($response);
+        } catch (\Throwable $e) {
+            $psr7->getWorker()->error((string)$e);
+        }
     }
-}
+})();
