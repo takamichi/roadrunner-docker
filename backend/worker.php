6fcba91a57a70b10c80d7bb8a80720a84abe1d4f
Takamichi Urata <taka@seraphimis.net>
2019-10-26 20:28:06 +0900

    Load .env file

--- backend/worker.php
+++ backend/worker.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
 (static function (): void {
+    $worker = new Spiral\RoadRunner\Worker(new Spiral\Goridge\StreamRelay(\STDIN, \STDOUT));
     $psr17 = new Nyholm\Psr7\Factory\Psr17Factory();
-    $psr7 = new Spiral\RoadRunner\PSR7Client(
-        new Spiral\RoadRunner\Worker(new Spiral\Goridge\StreamRelay(\STDIN, \STDOUT)),
-        $psr17,
-        $psr17,
-        $psr17
-    );
+    $psr7 = new Spiral\RoadRunner\PSR7Client($worker, $psr17, $psr17, $psr17);
+
+    // Load .env file
+    try {
+        Dotenv\Dotenv::create(__DIR__)->load();
+    } catch (Throwable $e) {
+        error_log((string)$e);
+        $worker->stop();
+        return;
+    }
 
     while ($request = $psr7->acceptRequest()) {
         try {
